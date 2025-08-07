9c2393419b15f866ea65d2fe2e4e16e2a39e3237
Shifatpreet <shifatpreetsingh@gmail.com>
2025-08-07 12:10:30 +0530

    Create Arrays in c plus plus.cpp

---
+++ Arrays in c plus plus.cpp
@@ -0,0 +1,21 @@
+//SHIFATPREET SINGH
+//ENTC B1
+//24070123098
+#include<iostream>
+using namespace std;
+int main(){
+    int num[10];
+    cout <<"Enter any 5 integer values: ";
+    for(int i=0;i<5;i++){
+        cin>>num[i];
+    }
+    for(int j=0;j<5;j++)
+    {
+        cout<<num[j]<<" ";
+    }
+}
+/*
+OUTPUT
+Enter any 5 integer values: 2 3 5 6 7
+2 3 5 6 7 
+*/
