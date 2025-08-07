a560033b611b1b60aafcc4dc0f8b7e1709b07107
Shifatpreet <shifatpreetsingh@gmail.com>
2025-08-07 15:00:43 +0530

    Create Declare and initialize an array.cpp

---
+++ Declare and intialise an array.cpp
@@ -0,0 +1,28 @@
+//SHIFATPREET SINGH 
+//24070123098
+//ENTC B1 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int arr[5] = {10, 20, 30, 40, 50};
+
+   
+    for (int i = 0; i < 5; i++) {
+        cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+
+    return 0;
+}
+/*
+OUTPUT
+arr[0] = 10
+arr[1] = 20
+arr[2] = 30
+arr[3] = 40
+arr[4] = 50
+
+*/
+
